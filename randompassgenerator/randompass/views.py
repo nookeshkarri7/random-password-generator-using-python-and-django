8ec0886f9b2fcf45885cd8400a9e021ffb487e50
nookesh <nookeshkarri7@gmail.com>
2021-09-12 14:41:35 +0530

    Created html,css files and python code to generate randompass and linked in urls.py

--- randompassgenerator/randompass/views.py
+++ randompassgenerator/randompass/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
-# Create your views here.
+from  django.http import  HttpResponse
+import random
+.
+def randompassgen(request):
+    alphabets = "ABCDEFGH45678IJKLM!@#$%NOPQRSTUVWXYZ!@#$%^&()1234567890"
+    inputNum=request.GET.get('selectlength')
+    if inputNum==None:
+        inputNum=0
+    else:
+        inputNum=int(inputNum)
+    output_random_pass=''
+    for i in range(int(inputNum)):
+        randomNum=random.randint(0,len(alphabets))
+        output_random_pass+=str(alphabets[randomNum])
+    randompassop={'ran':output_random_pass}
+    return render(request,'index.html',randompassop)
